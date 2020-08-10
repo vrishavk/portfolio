3b88c1b6b32ccce4152507925505cce7bd8b5147
vrishavk <khanalvrishav@gmail.com>
2020-08-10 13:29:04 +0545

    Updates

---
+++ precache-manifest.ee64978b042295dc79bb9ee34590bc92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb142b242a713462a0b363d4154d6dac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3ed77ec773de9742b02",
+    "url": "/portfolio/static/css/main.d42fbece.chunk.css"
+  },
+  {
+    "revision": "7789d5c4ac9a46852d84",
+    "url": "/portfolio/static/js/2.cda98272.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cda98272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ed77ec773de9742b02",
+    "url": "/portfolio/static/js/main.004be4fe.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6d89e4f71a2454e457b12ee7ef5ea42e",
+    "url": "/portfolio/static/media/logo.6d89e4f7.png"
+  },
+  {
+    "revision": "5c13a516aef277f2b44cddc351d4b4bf",
+    "url": "/portfolio/static/media/prof.5c13a516.jpg"
+  },
+  {
+    "revision": "15cca3ba08ae371b4259e78b60293abb",
+    "url": "/portfolio/static/media/vri.15cca3ba.jpg"
+  }
+]);
